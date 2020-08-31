3935cec752b75f0bf8b574cd05fb47778d7b459d
michaelk777 <36177814+michaelk777@users.noreply.github.com>
2020-08-31 09:44:44 -0400

    Update sim config with command args

--- sim_config.R
+++ sim_config.R
@@ -1,6 +1,29 @@
 library(tidyverse)
 library(memoise)
 
+capture <- commandArgs(trailingOnly = TRUE)
+
+opt1 = list(make_option(c("--input"), type = "character", default = "input.yml", dest = "input"))
+
+
+user_input <- function(name, argv) {
+  return(name %in% names(argv))
+}
+
+argv <- parse_args(OptionParser(option_list = opt1))
+
+if (user_input("input", argv)) {
+  input = argv$input 
+    if(file.exists(input) == T){
+      input.yaml <- yaml::read_yaml(input)
+    }else{
+      message('\n Input YAML not found \n')
+      break;
+    }
+  } else {
+  message('Cannot proceed without input yaml file. Please use "--input" flag .\n')
+}
+
 if(is.null(input.yaml$cluster) == T){
   message('\n Please determine processing method in input config file - Cant Proceed without that \n')
   break;
