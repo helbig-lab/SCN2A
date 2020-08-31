eb52d4f049cf86162b89b1eb0baa49c99ce43c7f
michaelk777 <36177814+michaelk777@users.noreply.github.com>
2020-08-31 11:13:21 -0400

    update pca_config.R with source files

--- pca_config.R
+++ pca_config.R
@@ -30,3 +30,30 @@ if (user_input("input", argv)) {
   } else {
   message('Cannot proceed without input yaml file. Please use "--input" flag .\n')
 }
+
+if(is.null(input.yaml$functional_variant) == T){
+  message('\n Please determine functional variant file in input config file - Cant Proceed without that \n')
+  break;
+ }
+
+if(is.null(input.yaml$pos_prop) == T){
+  message('\n Please determine positive propagation file in input config file - Cant Proceed without that \n')
+  break;
+ }
+
+if(is.null(input.yaml$pos_prop) == T){
+  message('\n Please determine negative propagation file in input config file - Cant Proceed without that \n')
+  break;
+ }
+  
+
+if(is.null(input.yaml$pca_dir) == T){
+  message('\n Please determine directory in input config file - Cant Proceed without that \n')
+  break;
+}
+else{
+  source(paste0(input.yaml$pca_dir, run_pheno_k3.R))
+  source(paste0(input.yaml$pca_dir, run_variant_k3.R))
+  source(paste0(input.yaml$pca_dir, pheno_pca_roc.R))
+  source(paste0(input.yaml$pca_dir, variant_pca_roc.R)) 
+}
